6539f9e27f9e726400207930d4ba869435082baa
Seth Skousen <seth.skousen@moneydesktop.com>
2014-05-28 21:13:10 -0600

    Fix hook stuff

--- hook.php
+++ hook.php
@@ -1,3 +1,9 @@
 <?php
+echo "<pre>";
+echo shell_exec("git fetch origin 2>&1");
+echo "<br />";
+echo shell_exec("git reset --hard origin/master 2>&1");
+echo "<br />";
 echo shell_exec("/usr/bin/git pull 2>&1");
+echo "</pre>";
 ?>
