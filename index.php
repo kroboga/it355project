71c18b2841174fea5a1a9c1b4c9cfb98dc32a5e0
Seth Skousen <seth.skousen@moneydesktop.com>
2014-05-29 21:11:49 -0600

    Add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<div class="starter-template">
+	<div class="jumbotron">
+		<h1>Under Construction</h1>
+		<p>Well, we are still working on the site, so just click the button below to get to the working page.</p>
+		<p><a class="btn btn-primary btn-lg" role="button" href="/lab4/people/">People</a></p>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
