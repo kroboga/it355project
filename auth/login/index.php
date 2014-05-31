b254db2dfa74cfb8a9f36f335b70cd073cdb8ba8
Seth Skousen <seth.skousen@moneydesktop.com>
2014-05-31 13:08:42 -0600

    Build Login

--- auth/login/index.php
+++ auth/login/index.php
@@ -2,5 +2,29 @@
 	$page = "Login";
 	include 'header.php'; 
 ?>
-	<p><a class="btn btn-primary btn-lg" role="button" href="doSubmit.php">Login!</a></p>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form role="form" action="doSubmit.php">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title" id="myModalLabel">Login</h4>
+				</div>
+				<div class="modal-body">					
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email address</label>
+						<input type="email" class="form-control" id="email" placeholder="E-mail">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="password" placeholder="Password">
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary">Login</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
 <?php include 'footer.php'; ?>
