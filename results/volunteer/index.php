cb7fc799af2f72af7bdbfa40cf5f490299dee63e
darksidemilk <ldscellist@Gmail.com>
2014-05-31 23:05:07 -0600

    volunteer search and results structure and started pages

--- results/volunteer/index.php
+++ results/volunteer/index.php
@@ -1,5 +1,5 @@
 <?php 
-	$page = "volunteer search";
+	$page = "volunteer results";
 	include "header.php"; 
 ?>
 	<!-- format of 1 row and 5 columns, we can change this as neccesarry, I felt it a good start -->
@@ -7,23 +7,12 @@
 		
 		<!-- column 1 - far left -->
 		<div class="col-md-4">
-
 		</div>
 
 		<!-- column 2 - center -->
 		<div class="col-md-4">
-			<h1>Volunteer Search</h1>
-			<p>Search for volunteers in specific areas and or with specific skills and contact them about helping out</p>
-
-			<!-- search bar -->
-			<form class="form-search">
-				<div class="form-group">
-					<input class="form-control" type="text">
-				</div>
-					<a class="btn btn-default" type="button" href="/results/volunteer">Search</a>
-			</form>
-
-
+			<h1>Volunteer search results</h1>
+			<p>This is some page content</p>
 		</div>
 
 		<!-- column 3 - right of center -->
