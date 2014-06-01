408a69bf275107c0c936b6cdf48f8b94446fa4f1
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-01 15:15:34 -0600

    Add footer

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,8 @@
+		<footer>
+			<p class="pull-right"><a href="#">Back to top</a></p>
+			<p>&copy; 2014 V-HUb, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+		</footer>
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="/js/bootstrap.min.js"></script>
 		<?php
