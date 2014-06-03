440940ae9e3e06bae535df62ecc7a5a4166e7eae
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-03 18:40:29 -0500

    can't get the picture to be ABOVE the buttons dang it....

--- dashboard/index.php
+++ dashboard/index.php
@@ -12,13 +12,12 @@
 	<div class="row">
 		<!-- column 1 - far left -->
 		<div class="col-md-4">
+			<img src="http://placehold.it/150x150">
 			<div class="btn-group-vertical">			
-				<img src="http://placehold.it/150x150">
 				<button type="button" class="btn btn-primary">Edit Profile</button>
 				<button type="button" class="btn btn-primary">Create Opportunity</button>
 				<button type="button" class="btn btn-primary">Inbox</button>
 			</div>
-
 		</div>
 
 		<!-- column 2 - center -->
