0d3374afdf5a09d1e93f480a2e8157e7927ca6a5
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-02 22:32:45 -0500

    adf

--- dashboard/index.php
+++ dashboard/index.php
@@ -12,8 +12,7 @@
 	<div class="row">
 		<!-- column 1 - far left -->
 		<div class="col-md-4">
-			
-			<ul>
+			<ul class="list-unstyled">
 				<li><img src="http://placehold.it/150x150"></li>
 				<li><button type="button" class="btn btn-primary">Edit Profile</button></li>
 				<li><button type="button" class="btn btn-primary">Create Opportunity</button></li>
