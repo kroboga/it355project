64d977a0a976a6bf61853d438e370167b13e8428
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-03 22:42:12 -0600

    About tweaks 2.0

--- about/index.php
+++ about/index.php
@@ -9,25 +9,28 @@
 	<!-- format of 1 row and 5 columns, we can change this as neccesarry, I felt it a good start -->
 	<div class="row">		
 		<!-- column 1 - far left -->
-		<div class="col-md-5">
+		<div class="col-md-4">
 			<img class="featurette-image img-responsive" src="/img/about.jpg" alt="Kitten praying">
 		</div>
 
 		<!-- column 2 - center -->
-		<div class="col-md-7">
+		<div class="col-md-8">
 			<h2>The Idea</h2>
 			<p>Helping people should make you feel as good as this picture of a kitten. In fact, it is praying that you will help people around you. You feel good for helping people, they feel good for getting helped. What's not to like? That</p>
 			<p>That's what we were thinking when we decided to start this site: lets make this kitten happy. Now we have over 3 vistors, and over 1 opportunity currently listed. That is called success.</p>
 		</div>
 
 		<div class="col-md-12">
+			<hr style="margin: 40px 0;">
 			<h2>The Beginning</h2>
 			<p>V-Hub.com was founded in early 2019 as a collaboration between Google and Apple Inc. There were also some students in there somewhere too, but I forget where. We decided people needed a place to 'meat' and volunteer. And on the subject of meat, here is some more bacon ipsum:</p>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>	
+			<hr style="margin: 40px 0;">
 			<h2>The Beginning Strikes Back</h2>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>
+			<hr style="margin: 40px 0;">
 			<h2>The Return of the End</h2>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>		
