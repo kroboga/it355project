a0acde59f76dfe9b82a42bb9ccb8930a9038ebfa
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-03 22:55:37 -0600

    About v42.3

--- about/index.php
+++ about/index.php
@@ -44,7 +44,6 @@
 	<hr style="margin: 40px 0;">
 	<div class="row">
 		<div class="col-md-12">
-			<hr style="margin: 40px 0;">
 			<h2>The Return of the End</h2>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>
 			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>		
