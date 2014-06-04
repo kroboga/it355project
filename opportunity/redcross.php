8ff6c95a0b2ebef752cc52d8ce0310fb3826e131
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-03 23:43:00 -0500

    more redcross stuff

--- opportunity/redcross.php
+++ opportunity/redcross.php
@@ -12,12 +12,22 @@
 		</div>
 	</div>
 	<div class="row">	
-		<div class="col-md-4">
-		</div>
-		<div class="col-md-6">
-			<h1>This is a page title</h1>
-			<p>This is some page content</p>
-			<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FDenver" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
+		<div class="col-md-offset-4">
+			<dl class="dl-horizontal">
+					<dt>Date & Time</dt>
+					<dd><iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;height=300&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FDenver" style=" border-width:0 " 
+						width="400" height="300" frameborder="0" scrolling="no"></iframe></dd>
+					<br />
+					<dt>Volunteers</dt>
+					<dd>33/100</dd>
+					<br />
+				 <dt>Gift of the Heart Exchange</dt>
+					<dd>Americans each discard an average of 68 pounds of clothes 
+					every year.  By donating your clothing to charity, you can 
+					put some of that to good use. Here are some tips on clearing 
+					out your closet and helping others enjoy your unwanted clothing 
+					and textiles...<a href="/opportunity/heartgift.php">Read More</a></dd>
+			</dl>			
 		</div>
 
 	
