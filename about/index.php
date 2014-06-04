dfdaa612bb1a0a8d5f1556757f16517774f9aa5a
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-03 22:36:29 -0600

    About Page

--- about/index.php
+++ about/index.php
@@ -1,11 +1,29 @@
-<?php include 'header.php'; ?>
+<?php 
+	$page = "About";
+	include "header.php"; 
+?>
 <div class="container">
-	<div class="starter-template">
-		<div class="jumbotron">
-			<h1>Under Construction</h1>
-			<p>Well, we are still working on the site, so just click the button below to get to the working page.</p>
-			<p><a class="btn btn-primary btn-lg" role="button" href="#">Click me!</a></p>
+	
+	<h1>About Us</h1>
+	<!-- format of 1 row and 5 columns, we can change this as neccesarry, I felt it a good start -->
+	<div class="row">		
+		<!-- column 1 - far left -->
+		<div class="col-md-5">
+			<img class="featurette-image img-responsive" src="/img/about.jpg" alt="Kitten praying">
 		</div>
+
+		<!-- column 2 - center -->
+		<div class="col-md-7">
+			<p>Helping people should make you feel as good as this picture of a kitten. In fact, it is praying that you will help people around you. You feel good for helping people, they feel good for getting helped. What's not to like? That</p>
+			<p>That's what we were thinking when we decided to start this site: lets make this kitten happy. Now we have over 3 vistors, and over 1 opportunity currently listed. That is called success.</p>
+		</div>
+
+		<div class="col-md-12">
+			<p>V-Hub.com was founded in early 2019 as a collaboration between Google and Apple Inc. There were also some students in there somewhere too, but I forget where. We decided people needed a place to 'meat' and volunteer. And on the subject of meat, here is some more bacon ipsum:</p>
+			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>
+			<p>Bacon ipsum dolor sit amet drumstick short ribs ground round turducken ham short loin pork loin. Flank fatback meatball hamburger chuck landjaeger sirloin shank capicola ham hock tail tongue beef jerky pork. Pork belly capicola pork chop, pig jerky strip steak corned beef ham hock fatback drumstick flank. Venison frankfurter jerky short ribs chuck biltong, ribeye meatloaf pork shankle kevin tongue bacon salami cow. Short loin cow pork belly, bresaola beef tri-tip tenderloin.</p>		
+		</div>
+	<!-- end of row -->
 	</div>
 </div>
 <?php include 'footer.php'; ?>
