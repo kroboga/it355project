c8c2f46fd4bcae7506063d7348094caecd5fada7
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-03 23:56:06 -0500

    all the red cross

--- opportunity/redcross.php
+++ opportunity/redcross.php
@@ -2,6 +2,25 @@
 	$page = "nothing";
 	include "header.php"; 
 ?>
+<style>
+      #map_canvas {
+        width: 500px;
+        height: 400px;
+      }
+</style>
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script>
+	function initialize() {
+        var map_canvas = document.getElementById('map_canvas');
+        var map_options = {
+          center: new google.maps.LatLng(44.5403, -78.5463),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(map_canvas, map_options)
+      }
+	google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 <div class="container">
 	<div class="row">
 		<div class="col-md-offset-4">
@@ -33,12 +52,16 @@
 					<dt>Volunteers</dt>
 					<dd>33/100</dd>
 					<br />
-				 <dt>Gift of the Heart Exchange</dt>
-					<dd>Americans each discard an average of 68 pounds of clothes 
-					every year.  By donating your clothing to charity, you can 
-					put some of that to good use. Here are some tips on clearing 
-					out your closet and helping others enjoy your unwanted clothing 
-					and textiles...<a href="/opportunity/heartgift.php">Read More</a></dd>
+				<dt>Location:</dt>
+					<dd><div id="map_canvas"></div></dd>
+				<dt>About:</dt>
+					<dd>he Red Cross responds to approximately 70,000 disasters 
+				in the United States every year, ranging from home fires 
+				that affect a single family to hurricanes that affect tens 
+				of thousands, to earthquakes that impact millions. In these events, 
+				the Red Cross provides. Brooklyn tattooed meh XOXO Tonx swag. Kogi Etsy put a bird on it brunch pop-up, direct trade mustache flexitarian sartorial. Sartorial kale chips fanny pack fap literally shabby chic, cardigan messenger bag keffiyeh food truck put a bird on it pug ennui Brooklyn. Cosby sweater art party stumptown High Life twee Marfa Truffaut Helvetica. Carles DIY farm-to-table typewriter whatever craft beer, fashion axe sartorial next level kale chips dreamcatcher four loko. Beard brunch master cleanse, single-origin coffee chambray chillwave slow-carb YOLO Shoreditch cray 90's freegan actually whatever Etsy. Tousled PBR&B tote bag +1 four loko kale chips.</dd>
+				<dt>What we need:</dt>
+					<dd>Pug craft beer blog, umami lo-fi cornhole gluten-free ennui bicycle rights messenger bag. Flannel fingerstache cardigan, keytar selvage Pinterest Schlitz authentic Thundercats organic actually. Letterpress bespoke Vice XOXO, meggings meh Bushwick authentic American Apparel kogi seitan. Street art forage Schlitz, put a bird on it Brooklyn Bushwick wolf messenger bag PBR&B food truck hella cred. IPhone Wes Anderson before they sold out normcore. Kogi readymade actually sriracha kale chips, literally Marfa farm-to-table Portland church-key 8-bit. Kickstarter leggings chambray, flexitarian sartorial tousled photo booth Marfa.</dd>
 			</dl>			
 		</div>
 	</div>
