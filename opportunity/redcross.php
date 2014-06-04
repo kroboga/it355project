a3c77a04e4b77a28ca118b93dc3cc5897f10e0b9
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-03 23:04:38 -0500

    added redcross stuff

---
+++ opportunity/redcross.php
@@ -0,0 +1,31 @@
+<?php 
+	$page = "template";
+	include "header.php"; 
+?>
+<div class="container">
+	
+	<!-- format of 1 row and 5 columns, we can change this as neccesarry, I felt it a good start -->
+	<div class="row">
+		
+		<!-- column 1 - far left -->
+		<div class="col-md-4">
+		</div>
+
+		<!-- column 2 - center -->
+		<div class="col-md-4">
+			<h1>This is a page title</h1>
+			<p>This is some page content</p>
+		</div>
+
+		<!-- column 3 - right of center -->
+		<div class="col-md-4">
+		</div>
+
+		<!-- column 4 - far right of center -->
+		<div class="col-md-4">
+		</div>
+	
+	<!-- end of row -->
+	</div>
+</div>
+<?php include 'footer.php'; ?>
