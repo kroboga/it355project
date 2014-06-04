303b7ac7584f6de1ca77471ac0976c78c3055599
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-04 00:03:03 -0600

    Register2

--- auth/register/index.php
+++ auth/register/index.php
@@ -2,35 +2,35 @@
 	$page = "template";
 	include "header.php"; 
 ?>
-<div class="container">
-	
-<div class="row">
-		<div class="col-md-6">
-			<form class="form-horizontal" role="form" _lpchecked="1" action="/dashboard/" method="post">
-				<div class="input-group">
-					<span class="input-group-addon">Name:</span>
-					<input id="name" type="text" class="form-control" placeholder="Location">
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form role="form" action="/dashboard/">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myModalLabel">Register</h4>
 				</div>
-				<br>
-				<div class="input-group">
-					<span class="input-group-addon">Email:</span>
-					<input id="email" type="number" class="form-control" value="10" min="1" step="1" pattern="\d+">
+				<div class="modal-body">
+					<div class="form-group">
+						<label for="exampleInputEmail1">Name</label>
+						<input type="email" class="form-control" id="email" placeholder="Name">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email address</label>
+						<input type="email" class="form-control" id="email" placeholder="E-mail">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="password" placeholder="Password">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Confirm</label>
+						<input type="password" class="form-control" id="confirm" placeholder="Confirm">
+					</div>
 				</div>
-				<br>
-				<div class="input-group">
-					<span class="input-group-addon">Password:</span>
-					<input id="password" type="password" class="form-control" placeholder="Name">
-				</div>
-				<br>
-				<div class="input-group">
-					<span class="input-group-addon">Confirm:</span>
-					<input id="confirm" type="password" class="form-control" placeholder="Name">
-				</div>
-				<div class="input-group">
-					<button class="btn btn-primary" type="submit">Edit Profile</button>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary">Login</button>
 				</div>
 			</form>
-			<br>
 		</div>
 	</div>
 </div>
