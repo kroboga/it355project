5487306d049b2c191c30262e291a7787cc2b850e
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-03 23:37:50 -0500

    red cross stuff

--- opportunity/redcross.php
+++ opportunity/redcross.php
@@ -1,29 +1,25 @@
 <?php 
-	$page = "template";
+	$page = "nothing";
 	include "header.php"; 
 ?>
 <div class="container">
 	
 	<!-- format of 1 row and 5 columns, we can change this as neccesarry, I felt it a good start -->
 	<div class="row">
-		
-		<!-- column 1 - far left -->
+		<div class="col-md-offset-4">
+			<h1>Red Cross</h1>
+			<br />
+		</div>
+	</div>
+	<div class="row">	
 		<div class="col-md-4">
 		</div>
-
-		<!-- column 2 - center -->
-		<div class="col-md-4">
+		<div class="col-md-6">
 			<h1>This is a page title</h1>
 			<p>This is some page content</p>
+			<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=en.usa%23holiday%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FDenver" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
 		</div>
 
-		<!-- column 3 - right of center -->
-		<div class="col-md-4">
-		</div>
-
-		<!-- column 4 - far right of center -->
-		<div class="col-md-4">
-		</div>
 	
 	<!-- end of row -->
 	</div>
