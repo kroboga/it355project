ab77818364e702a74cd4d3521ec0770bd243c893
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-04 09:31:37 -0600

    Bootstrap.js in header

--- include/header.php
+++ include/header.php
@@ -24,6 +24,8 @@
    		<link rel="stylesheet" type="text/css" media="all" href="/css/daterangepicker-bs3.css" />
 	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		
 		<script>
 			function showLogin()
 			{
