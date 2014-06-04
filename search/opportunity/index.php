291bc5b4cbe361e2660cd55730e1b82e1f3a3643
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-03 21:29:27 -0500

    MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP

--- search/opportunity/index.php
+++ search/opportunity/index.php
@@ -13,9 +13,9 @@
       function initialize() {
         var map_canvas = document.getElementById('map_canvas');
         var map_options = {
-          center: new google.maps.LatLng(44.5403, -78.5463),
+          center: new google.maps.LatLng(39.50, -98.35),
           zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+          mapTypeId: google.maps.MapTypeId.HYBRID
         }
         var map = new google.maps.Map(map_canvas, map_options)
       }
@@ -28,11 +28,21 @@
 			<h1>Find Service Opportunities</h1>
 		</div>
 	</div>
-	<div class="col-md-6">
-		<div id="map_canvas"></div>
+	<div class="row">
+		<div class="col-md-6">
+			<p>stuff</p>
+		</div>
+		<div class="col-md-6">
+			<p>stuff</p>
+		</div>
 	</div>
-	<div class="col-md-6">
-		<h2>Results:</h2>
+	<div class="row">
+		<div class="col-md-6">
+			<div id="map_canvas"></div>
+		</div>
+		<div class="col-md-6">
+			<h2>Results:</h2>
+		</div>
 	</div>
 </div>
 <?php include 'footer.php'; ?>
