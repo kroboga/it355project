5e378de131a3e53760464a459ce8bd8476706054
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-04 00:39:22 -0600

    View Profile

--- profile/edit/index.php
+++ profile/edit/index.php
@@ -1,31 +1,31 @@
 <?php 
-	$page = "template";
 	include "header.php"; 
 ?>
 <div class="container">
-	
-	<!-- format of 1 row and 5 columns, we can change this as neccesarry, I felt it a good start -->
 	<div class="row">
-		
-		<!-- column 1 - far left -->
+		<div class="col-md-12">
+			<h1>Frank Frankenbotham</h1>
+		</div>	
+	</div>
+	<div class="row">
 		<div class="col-md-4">
+			<h3>Photo</h2>
+			<img class="featurette-image img-responsive img-rounded" src="/img/profile.jpg" alt="Frank Frankenbotham">
+			<p><a class="btn btn-lg btn-primary" href="#" role="button">Upload Picture</a></p>
 		</div>
-
-		<!-- column 2 - center -->
 		<div class="col-md-4">
-			<h1>This is a page title</h1>
-			<p>This is some page content</p>
+			<h3>Profile</h2>
 		</div>
-
-		<!-- column 3 - right of center -->
 		<div class="col-md-4">
+			<h3>Skills</h2>
+			<p>nunChuck skills</p>
+			<p>Manual Labor Experience</p>
+			<p>Web Development</p>
+			<p>Theater Sound Tech</p>
+			<p>Microsoft Office</p>
+			<p>Hospice Experience</p>
+			<p>Eagle Scout</p>
 		</div>
-
-		<!-- column 4 - far right of center -->
-		<div class="col-md-4">
-		</div>
-	
-	<!-- end of row -->
 	</div>
 </div>
 <?php include 'footer.php'; ?>
