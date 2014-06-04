a6e7d84469a18e4e6636ace1b81545d1f55a1378
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-03 21:04:14 -0500

    maps... y u no work

--- search/opportunity/index.php
+++ search/opportunity/index.php
@@ -4,11 +4,6 @@
 ?>
 <div class="container">
 	<div class="starter-template">
-		<div class="jumbotron">
-			<h1>V-Hub</h1>
-			<p>Well, we are still working on the site, so just click the button below to get to the working page.</p>
-			<p><a class="btn btn-primary btn-lg" role="button" href="#">Click me!</a></p>
-		</div>
 		<div>
 			 <script src="https://maps.googleapis.com/maps/api/js"></script>
 		</div>
