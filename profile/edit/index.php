70a160d49eb7a070892ae252dac78ce34a77a49b
darksidemilk <ldscellist@Gmail.com>
2014-06-11 02:33:34 -0600

    edit profile, wrong dropdown

--- profile/edit/index.php
+++ profile/edit/index.php
@@ -11,66 +11,97 @@
 	<div class="row" style="min-height:500px">
 		<div class="col-md-4">
 			<img class="img-responsive img-rounded" src="/img/profile.jpg" alt="Frank Frankenbotham" style="margin:auto; padding:1em;">
-			<p><a class="btn btn-lg btn-primary" href="#" role="button">Upload Picture</a></p>
+			<p><a class="btn btn-lg btn-primary" onclick="alert('upload dialog box')" role="button">Upload Picture</a></p>
 		</div>
 		<div class="col-md-4" style="text-align:left">
 			<h3>Profile</h3>
-			<p>Location: Provo, UT</p>
-			<p>Age: 20-30</p>
-			<p>Volunteer/Organizer</p>
+
+			<div class="form-group">
+					Location: <input class="form-control" type="text" placeholder="zip code">
+			</div>
+			
+			<div class="dropdown">
+			  <a id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+			    Age Range
+			  <span class="caret"></span>
+			  </a>
+			  <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+			    <li> 16-20 </li>
+			  </ul>
+			</div>
+
+
+			<div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Volunteer
+				    </label>
+			    </div>
+		    <div class="checkbox">
+			    <label>
+			      <input type="checkbox"> Organizer
+			    </label>
+		    </div>
+		    <div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Both
+				    </label>
+			    </div>
+
+
 			<p>Member since: Jan 1, 1970</p>
 		</div>
+
 		<div class="col-md-4" style="text-align:left">
 			<h3>Skills</h3>
-			<div class="checkbox">
-							    <label>
-							      <input type="checkbox"> Computer Skills
-							    </label>
-						    </div>
+				<div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Computer Skills
+				    </label>
+			    </div>
 
-							<div class="checkbox">
-							    <label>
-							      <input type="checkbox"> Manual Labor Experience
-							    </label>
-						    </div>
+				<div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Manual Labor Experience
+				    </label>
+			    </div>
 
-							<div class="checkbox">
-							    <label>
-							      <input type="checkbox"> Theater Sound Tech
-							    </label>
-						    </div>
+				<div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Theater Sound Tech
+				    </label>
+			    </div>
 
-							<div class="checkbox">
-							    <label>
-							      <input type="checkbox"> EMT
-							    </label>
-						    </div>
+				<div class="checkbox">
+				    <label>
+				      <input type="checkbox"> EMT
+				    </label>
+			    </div>
 
-							<div class="checkbox">
-							    <label>
-							      <input type="checkbox"> Hospice
-							    </label>
-						    </div>
+				<div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Hospice
+				    </label>
+			    </div>
 
-						    <div class="checkbox">
-							    <label>
-							      <input type="checkbox"> Web Development
-							    </label>
-						    </div>
+			    <div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Web Development
+				    </label>
+			    </div>
 
-						    <div class="checkbox">
-							    <label>
-							      <input type="checkbox"> Microsoft Office
-							    </label>
-						    </div>
+			    <div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Microsoft Office
+				    </label>
+			    </div>
 
-						    <div class="checkbox">
-							    <label>
-							      <input type="checkbox"> Scouting
-							    </label>
-						    </div>
+			    <div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Scouting
+				    </label>
+			    </div>
 						    
-						   		</div>
+   		</div>
 	</div>
 </div>
 <?php include 'footer.php'; ?>
