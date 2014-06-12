fc98cee884f0f8d6270362ad68751792af534bd8
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-12 00:07:12 -0600

    messages .9

---
+++ message/send/index.php
@@ -0,0 +1,41 @@
+<?php 
+	$page = "Dashboard";
+	include "header.php"; 
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Send a Message</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<form role="form">
+				<div class="form-group">
+					<label for="to">To:</label>
+					<input type="email" class="form-control" id="to" placeholder="Frank Frankenbotham &lt;frank@frank.com&gt;">
+				</div>
+				<div class="form-group">
+					<label for="to">Subject:</label>
+					<input type="email" class="form-control" id="subject" placeholder="Frank Frankenbotham &lt;frank@frank.com&gt;">
+				</div>
+				<div class="form-group">
+					<label for="to">Message:</label>
+					<textarea class="form-control" rows="3"></textarea>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-1">
+			<a class="btn btn-lg btn-primary " href="/dashboard/" role="button">Send</a>
+		</div>
+		<div class="col-md-10">
+			&nbsp;
+		</div>
+		<div class="col-md-1">
+			<a class="btn btn-lg btn-primary " href="/dashboard/" role="button">Cancel</a>
+		</div>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
