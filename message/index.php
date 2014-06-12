96fd8f12ab908b86b7e13eecccc0d33d82075447
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-12 00:25:24 -0600

    message v1.9

--- message/index.php
+++ message/index.php
@@ -3,29 +3,71 @@
 	include "header.php"; 
 ?>
 <div class="container">
-	
-	<!-- format of 1 row and 5 columns, we can change this as neccesarry, I felt it a good start -->
-	<div class="row">
-		
-		<!-- column 1 - far left -->
-		<div class="col-md-4">
+<div class="row">
+		<div class="col-md-12">
+			<h1>Message Inbox</h1>
 		</div>
-
-		<!-- column 2 - center -->
-		<div class="col-md-4">
-			<h1>This is a page title</h1>
-			<p>This is some page content</p>
-		</div>
-
-		<!-- column 3 - right of center -->
-		<div class="col-md-4">
-		</div>
-
-		<!-- column 4 - far right of center -->
-		<div class="col-md-4">
-		</div>
-	
-	<!-- end of row -->
 	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<table class="table table-striped">
+					<tr>
+						<th>Time</th>
+						<th>From</th>
+						<th>Subject</th>
+						<th>Delete</th>
+					</tr>
+					<tr>
+						<td>001</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">Volunteering into Darkness</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+					<tr>
+						<td>001</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">Volunteering: First Contact</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+					<tr>
+						<td>001</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">Volunteer Generations</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+					<tr>
+						<td>001</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">The Undiscoverd Volunteer</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+					<tr>
+						<td>001</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">The Final Volunteer</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+					<tr>
+						<td>001</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">The Voyage to the Volunteer</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+					<tr>
+						<td>002</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">The Search for Volunteers</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+					<tr>
+						<td>003</td>
+						<td>Frank Frankenbotham</td>
+						<td><a href="/message/view/">The Wrath of the Volunteer</a></td>
+						<td><a href="#"><span class="glyphicon glyphicon-trash"></span></a></td>
+					</tr>
+			</table>
+		</div>
+	</div>
+
 </div>
 <?php include 'footer.php'; ?>
