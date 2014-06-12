3a90ed8ef4da35469b40a128fb26859f3751311a
Seth Skousen <seth.skousen@moneydesktop.com>
2014-06-11 19:29:49 -0600

    CheckLogin

--- auth/login/index.php
+++ auth/login/index.php
@@ -1,5 +1,11 @@
 <?php
 	session_start();
 	$_SESSION['loggedin'] = true;
+	if (!is_null($_SESSION['loginRedirect']) && trim($_SESSION['loginRedirect']))
+	{
+		$temp = $_SESSION['loginRedirect']);
+		$_SESSION['loginRedirect']) = "";
+		header("Location: ".$temp)
+	}
 	header("Location: /");
 ?>
---
+++ include/checkLogin.php
@@ -0,0 +1,9 @@
+<?php
+	session_start();
+	if (is_null($_SESSION['loggedin']) || !$_SESSION['loggedin'];)
+	{
+		$_SESSION['loginRedirect'] = $_SERVER['REQUEST_URI'];
+		header("Location: /auth/login/");
+		exit();
+	}
+?>
