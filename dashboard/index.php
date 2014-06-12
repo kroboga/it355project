adaaa706884092d842859d66f1205ec6862fe652
darksidemilk <ldscellist@Gmail.com>
2014-06-11 19:32:16 -0600

    rawr

--- dashboard/index.php
+++ dashboard/index.php
@@ -84,7 +84,7 @@
 							<div class="btn-group">
 								<button class="btn btn-primary" type="submit" formaction="/opportunity/heartgift.php">Edit Opportunity</button>
 								<button class="btn btn-primary" type="submit" formaction="/messaging/allvolunteers.php">Message All Volunteers</button>
-								<button class="btn btn-primary" type="submit" formaction="/messaging/allvolunteers.php">View Volunteers</button>
+								<button class="btn btn-primary" type="submit" formaction="/results/volunteer/">View Volunteers</button>
 								<button class="btn btn-primary" type="submit" formaction="/search/volunteer/">Find Volunteers for this Opportunity</button>
 							</div>
 						</form>
