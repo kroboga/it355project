8f7e93518febd09485a5426158c4bd5513d03545
Kirk Johnson <kirk.johnson@goparakeet.com>
2014-06-11 20:05:17 -0600

    ;lkhgsaertyui

--- createopp/index.php
+++ createopp/index.php
@@ -21,7 +21,7 @@
 			<div id="datetime" class="form-group">
 				<div class="checkbox">
 					<label>
-						<input type="checkbox"> Reccurring Event
+						<input type="checkbox" id="event" value="event"> Reccurring Event
 					</label>
 				</div>
 				<label for="start" class="col-sm-2 control-label">Start Date:</label>
